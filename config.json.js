ffeabb1b76ae5a3b2df7dcbf2847d0bb58b0d3a2
WangH <WangH_dev@example.com>
2025-09-08 11:01:06 +0800

    bump: update OpenList version to v4.1.2

--- config.json.js
+++ config.json.js
@@ -3,9 +3,9 @@
     "description": "OpenList 一个支持多种存储的文件列表程序，使用 Gin 和 Solidjs", 
     "home_url": "Module_openlist.asp", 
     "link": "https://github.com/everstu/Koolcenter_openlist", 
-    "md5": "cec2ea779dcf3c0654f4f11063f63300", 
+    "md5": "126d47dd58df8762e9c857d2c79c5683", 
     "module": "openlist", 
     "tags": "云同步", 
     "title": "OpenList 文件列表", 
-    "version": "4.1.1-1"
+    "version": "4.1.2"
 }
