d96d369e434905584ff0ded080429b890e29e59a
WangH <WangH@example.com>
2026-06-08 11:07:44 +0800

    bump bin ver 4.2.2

--- config.json.js
+++ config.json.js
@@ -3,7 +3,7 @@
     "description": "OpenList 一个支持多种存储的文件列表程序，使用 Gin 和 Solidjs", 
     "home_url": "Module_openlist.asp", 
     "link": "https://github.com/everstu/Koolcenter_openlist", 
-    "md5": "c6f5d5d84724135115bd2cee71bc3e9a", 
+    "md5": "8c295a73c7c5d591a827b5b384780e84", 
     "module": "openlist", 
     "tags": "云同步", 
     "title": "OpenList 文件列表", 
