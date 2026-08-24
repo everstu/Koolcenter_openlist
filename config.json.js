d60b81f828bd1cf59c79d6f5e13844834a22b3bc
tz-wesley <wengheng@lifebanktech.com>
2026-08-24 17:34:01 +0800

    bump bin ver to v4.2.5

--- config.json.js
+++ config.json.js
@@ -3,9 +3,9 @@
     "description": "OpenList 一个支持多种存储的文件列表程序，使用 Gin 和 Solidjs", 
     "home_url": "Module_openlist.asp", 
     "link": "https://github.com/everstu/Koolcenter_openlist", 
-    "md5": "782f427c92a01227a708d56a0ed29639", 
+    "md5": "1436cd51776def7101ad3a2cbdf5007e", 
     "module": "openlist", 
     "tags": "云同步", 
     "title": "OpenList 文件列表", 
-    "version": "4.2.4"
+    "version": "4.2.5"
 }
